acf1912958d47170d0f906b394ca6fa12d18ace8
Jon Prusik <jprusik@classynemesis.com>
2019-04-07 13:56:05 -0400

    replace author header text with author svg logo

--- src/AuthorSummary.js
+++ src/AuthorSummary.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { arrayOf, string, object } from 'prop-types';
 import SocialLinks from './SocialLinks';
 import './AuthorSummary.scss';
+import { ReactComponent as Logo } from './logo.svg';
 
 export default function AuthorSummary(props) {
   const { currentRoles, funFacts, locationBasis, links, name } = props;
 
   return [
-    <h1 key="author-name" className="author-name">{name}</h1>,
+    <Logo key="author-logo" className="author-logo" title={name} />,
     <div key="author-roles" className="author-roles">
       { currentRoles.map(role => <div key={ role }>{ role }</div>) }
     </div>,
