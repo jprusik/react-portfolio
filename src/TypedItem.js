2b64fdc5f5722a3e4182c710b60cfa57cf8330b5
Jon Prusik <jprusik@classynemesis.com>
2019-04-09 19:15:33 -0400

    refactor TypedList into context-sensitive components

---
+++ src/TypedItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { string, number } from 'prop-types';
+import ExternalDomainLink from './ExternalDomainLink';
+
+export default function TypedItem(props) {
+  const { item, itemNumber, itemType } = props;
+
+  switch (itemType) {
+    case 'links':
+      return <ExternalDomainLink href={item}>{`link ${itemNumber+1}`}</ExternalDomainLink>;
+    case 'samples':
+      if (item === 'on-request') {
+        return 'available on request';
+      }
+
+      return <a href={require(`./content/${item}`)}>{`sample ${itemNumber+1}`}</a>;
+    default:
+      return item;
+  }
+}
+
+TypedItem.propTypes = {
+  item: string.isRequired,
+  itemNumber: number.isRequired,
+  itemType: string.isRequired
+};
--- src/TypedList.js
+++ src/TypedList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { isEmpty } from 'lodash';
 import { arrayOf, string, number } from 'prop-types';
-import ExternalDomainLink from './ExternalDomainLink';
+import TypedItem from './TypedItem';
 import './TypedList.scss';
 
 export default function TypedList(props) {
@@ -16,10 +16,7 @@ export default function TypedList(props) {
   const typedListHeader = headerText && <HeaderTag key={`${listType}-list-header`} className={`${listType}-list-header`}>{ headerText }</HeaderTag>;
   const formattedListItems = listItems.map((item, index) => (
     <li key={item} className="typed-list">
-      { ['links', 'samples'].indexOf(listType) > -1 ?
-        <ExternalDomainLink href={item}>{ `${listType} ${index+1}` }</ExternalDomainLink>
-        : item
-      }
+      <TypedItem item={item} itemNumber={index} itemType={listType} />
     </li>
   ));
 
