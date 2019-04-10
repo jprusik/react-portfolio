7f1ede10db5e2da763f6e8c6b0ef86622e142ba2
Jon Prusik <jprusik@classynemesis.com>
2019-04-09 22:16:46 -0400

    remove unneeded components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import React, { Component } from 'react';
 import AuthorSummary from './AuthorSummary';
 import PortfolioNav from './PortfolioNav';
 import PortfolioTimeline from './PortfolioTimeline';
-import './App.scss';
 import data from './content/data.json';
 
 class App extends Component {
   render() {
     const { author, projects, organizations, education } = data;
 
-    return (
-      <div className="app">
-        <div className="app-body">
-          <AuthorSummary { ...author }/>
-          <PortfolioNav projects={ projects } />
-          <PortfolioTimeline projects={ projects } organizations={ organizations } education= { education } />
-        </div>
-      </div>
-    );
+    return [
+      <AuthorSummary key="author-summary" { ...author }/>,
+      <PortfolioNav key="portfolio-nav" projects={ projects } />,
+      <PortfolioTimeline key="portfolio-timeline" projects={ projects } organizations={ organizations } education= { education } />
+    ];
   }
 }
 
