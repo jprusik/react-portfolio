10ee15a4c5492d293e8a90fd10ce0330284e0cb2
Jon Prusik <jprusik@classynemesis.com>
2019-04-14 15:57:38 -0400

    update footer with link to react website

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Footer.scss';
+import ExternalDomainLink from './ExternalDomainLink';
 
 export default function Footer() {
   return (
     <div className="app-footer">
       <p className="print-show">Check out the online version of this portfolio with expanded features, at&nbsp;<strong>{ process.env.REACT_APP_PRETTY_DOMAIN_URL }</strong></p>
+      <p className="print-hide">This site was written in <ExternalDomainLink href="https://reactjs.org/">React</ExternalDomainLink>. Check out the code <ExternalDomainLink href="https://github.com/jprusik/react-portfolio">here</ExternalDomainLink>.</p>
     </div>
   );
 }
