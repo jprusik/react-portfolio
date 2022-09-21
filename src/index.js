af9a637da2c0ce6dc14954d6536a5ed99f82a1b9
Jon Prusik <jprusik@classynemesis.com>
2022-09-20 20:50:50 -0400

    update React and other packages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +8,8 @@ import 'typeface-aleo';
 import 'typeface-ubuntu-condensed';
 import 'typeface-ubuntu-mono';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
